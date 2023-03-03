b8151827cca07f129cdcf49a92ce002884849c88
Samee <abdussamee602@gmail.com>
2023-03-03 00:36:04 -0500

    .

---
+++ src/main/java/com/tempu/tempeeu/TempeeuApplication.java
@@ -0,0 +1,13 @@
+package com.tempu.tempeeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempeeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempeeuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tempu/tempeeu/TempeeuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tempu.tempeeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempeeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
