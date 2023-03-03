c5845c4c56f5483529085ea4dbe4732ede7c8a5d
Samee <abdussamee602@gmail.com>
2023-03-03 00:44:42 -0500

    .

--- src/main/java/com/tempu/tempeeu/TempeeuApplication.java
+++ src/main/java/com/tempu/tempeeu/TempeeuApplication.java
@@ -2,9 +2,11 @@ package com.tempu.tempeeu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class TempeeuApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(TempeeuApplication.class, args);
